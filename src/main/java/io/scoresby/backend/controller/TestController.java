9c12ba94bdc2e425d43faedad089452e9f522a88
Scoresby <andresdaniel@gmail.com>
2022-05-25 20:55:35 +0200

    DAO

--- src/main/java/io/scoresby/backend/controller/TestController.java
+++ src/main/java/io/scoresby/backend/controller/TestController.java
@@ -2,6 +2,8 @@ package io.scoresby.backend.controller;
 
 import io.scoresby.backend.dao.Artist;
 import io.scoresby.backend.service.TestService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +15,14 @@ import java.util.List;
 @RequestMapping("api/v1")
 public class TestController {
 
+    private static final Logger log = LogManager.getLogger(TestController.class);
+
     @Autowired
     TestService testService;
 
     @GetMapping("/test")
     String testController() {
+        log.info("Test Controller");
         return "controller Test ok";
 
 
@@ -29,7 +34,6 @@ public class TestController {
 
 
     }
-
 
 
 }
